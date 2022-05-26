9d190caf5a6326ffe217101f8038a99d2068aae4
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-05-26 22:53:14 +0700

    feat: add options to add cli command

---
+++ maybe_later/console/application.py
@@ -0,0 +1,28 @@
+import click
+
+from . import utils
+
+
+@click.group()
+@utils.make_sync
+async def main():
+    pass
+
+
+@main.command(help="Download an article from a given URL")
+@click.option("-u", "--url", type=str, help="Article's URL")
+@click.option(
+    "-c",
+    "--category",
+    type=str,
+    help="Category and subcategory for the article. Could be in the form of 'category/subcategory' or just 'category'",
+)
+@click.option(
+    "-t",
+    "--tags",
+    type=str,
+    help="Comma-separated list of tags for the article. Could be in the form of 'tag1,tag2,tag3' or just 'tag1'",
+)
+@utils.make_sync
+async def add():
+    pass
