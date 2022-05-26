1049495a231bb652dd3b51d63b5b09d0dbd42d2b
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-05-26 23:37:56 +0700

    feat: add --save-images flag to add

--- maybe_later/console/application.py
+++ maybe_later/console/application.py
@@ -15,15 +15,25 @@ async def main():
     "-c",
     "--category",
     type=str,
+    default="",
     help="Category and subcategory for the article. Could be in the form of 'category/subcategory' or just 'category'",
 )
 @click.option(
     "-t",
     "--tags",
+    default="",
     type=str,
     help="Comma-separated list of tags for the article. Could be in the form of 'tag1,tag2,tag3' or just 'tag1'",
 )
+@click.option(
+    "--save-images",
+    type=bool,
+    is_flag=True,
+    default=False,
+    help="Save images locally or not",
+)
 @utils.make_sync
-async def add(url: str, category: str, tags: str):
+async def add(url: str, category: str, tags: str, save_images: bool):
     main_category, subcategory = utils.get_categories(category)
     article_tags = utils.get_tags(tags)
+    print(save_images)
