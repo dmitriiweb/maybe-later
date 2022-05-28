ae06e4d78a5b8626b6bd772073ab73f3374814aa
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-05-29 00:04:51 +0700

    feat: add status field to Article model

--- maybe_later/models/article.py
+++ maybe_later/models/article.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass, field
+from enum import Enum, auto
 from typing import List, Optional
+
+
+class ArticleStatus(Enum):
+    NEW = auto()
 
 
 @dataclass
@@ -11,6 +16,7 @@ class Article:
     category: Optional[str]
     subcategory: Optional[str]
     tags: List[str] = field(default_factory=list)
+    status: ArticleStatus = ArticleStatus.NEW
 
     def to_markdown(self) -> str:
         pass
