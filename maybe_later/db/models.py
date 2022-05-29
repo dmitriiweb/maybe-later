68eedca1c14453d6e2f0a76d97a15f1fe10553ba
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-05-29 18:53:33 +0700

    feat: add MetaCategoryLink model

--- maybe_later/db/models.py
+++ maybe_later/db/models.py
@@ -3,6 +3,30 @@ from typing import List, Optional
 from sqlmodel import Field, Relationship, SQLModel
 
 
+class MetaTagLink(SQLModel, table=True):
+    tag_id: Optional[int] = Field(default=None, foreign_key="tag.id", primary_key=True)
+    meta_id: Optional[int] = Field(
+        default=None, foreign_key="meta.id", primary_key=True
+    )
+
+
+class MetaCategoryLink(SQLModel, table=True):
+    tag_id: Optional[int] = Field(default=None, foreign_key="tag.id", primary_key=True)
+    category_id: Optional[int] = Field(
+        default=None, foreign_key="category.id", primary_key=True
+    )
+
+
+class Tag(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, default=None)
+    name: str = Field(sa_column_kwargs={"unique": True})
+
+    metas: List["Meta"] = Relationship(back_populates="tags", link_model=MetaTagLink)
+    categories: List["Category"] = Relationship(
+        back_populates="tags", link_model=MetaCategoryLink
+    )
+
+
 class SubCategory(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True, default=None)
     name: str
@@ -22,20 +46,9 @@ class Category(SQLModel, table=True):
 
     meta_id: Optional[int] = Field(default=None, foreign_key="meta.id")
     meta: Optional["Meta"] = Relationship(back_populates="category")
-
-
-class MetaTagLink(SQLModel, table=True):
-    tag_id: Optional[int] = Field(default=None, foreign_key="tag.id", primary_key=True)
-    meta_id: Optional[int] = Field(
-        default=None, foreign_key="meta.id", primary_key=True
+    tags: List[Tag] = Relationship(
+        back_populates="categories", link_model=MetaCategoryLink
     )
-
-
-class Tag(SQLModel, table=True):
-    id: Optional[int] = Field(primary_key=True, default=None)
-    name: str = Field(sa_column_kwargs={"unique": True})
-
-    metas: List["Meta"] = Relationship(back_populates="tags", link_model=MetaTagLink)
 
 
 class Meta(SQLModel, table=True):
