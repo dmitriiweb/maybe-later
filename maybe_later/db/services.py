d67ae65a4e8c7ed8d203fb0194ac88179db79cbd
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-05-30 17:55:32 +0700

    refactor: improve services functions

--- maybe_later/db/services.py
+++ maybe_later/db/services.py
@@ -14,41 +14,30 @@ from . import models
 SelectOfScalar.inherit_cache = True  # type: ignore
 Select.inherit_cache = True  # type: ignore
 
-UniqueModels = Union[models.Tag, models.Category]
+ModelsWithName = Union[models.Tag, models.Category]
 
 
 async def add_new_meta(article_meta: ArticleMeta, db_uri: str):
     await models.init_db(db_uri)
-    tags_tasks = [get_tag(i, db_uri) for i in article_meta.tags]
+    tags_tasks = [
+        get_model_by_name_or_create(i, models.Tag, db_uri) for i in article_meta.tags
+    ]
     tags = await asyncio.gather(*tags_tasks)
     print(f"{tags=}")
 
 
-async def get_tag(tag_name: str, db_uri: str) -> models.Tag:
-
+async def get_model_by_name_or_create(
+    name: str, model: Type[ModelsWithName], db_uri: str
+) -> ModelsWithName:
     engine = models.get_engine(db_uri)
-
     async with AsyncSession(engine) as session:
-        tag = await _get_by_name(tag_name, session, models.Tag)
-
-    if tag is not None:
-        return tag
-
-    tag = models.Tag(name=tag_name)
-    session.add(tag)
-    await session.commit()
-    await session.refresh(tag)
-    return tag
-
-
-async def get_category(category_name: str, db_uri: str) -> models.Category:
-    session = await models.get_session(db_uri)
-
-
-async def _get_by_name(
-    name: str, session: AsyncSession, model: Type[UniqueModels]
-) -> Optional[UniqueModels]:
-    stmt = select(model).where(model.name == name)
-    db_res = await session.execute(stmt)
-    res = db_res.first()
-    return res
+        stmt = select(model).where(model.name == name)
+        db_res = await session.execute(stmt)
+        res = db_res.first()
+        if res is not None:
+            return res  # type: ignore
+        m = model(name=name)
+        session.add(m)
+        await session.commit()
+        await session.refresh(m)
+        return m
