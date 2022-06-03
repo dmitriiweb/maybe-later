3750ddc270ce3f75364ed654ad7df76e5ae2cec2
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-06-03 18:34:29 +0700

    feat: color output

--- maybe_later/console/outputs.py
+++ maybe_later/console/outputs.py
@@ -1,7 +1,9 @@
 from typing import List
 
 from tabulate import tabulate
+from termcolor import colored
 
+from maybe_later._types import ArticleStatus
 from maybe_later.db.models import Meta
 
 
@@ -17,15 +19,38 @@ def string_shorter(text: str, max_length: int = 50) -> str:
     return f"{text[:max_length]}..."
 
 
+def color_text(text: str, color: str) -> str:
+    return colored(text, color)
+
+
+def color_factory(status: ArticleStatus):
+    colors = {
+        ArticleStatus.UNREAD: "green",
+        ArticleStatus.READ: "gray",
+        ArticleStatus.IMPORTANT: "red",
+    }
+    try:
+        return colors[status]
+    except KeyError:
+        raise ValueError(f"Unknown status: {status}")
+
+
+def make_meta_row(meta: Meta) -> List[str]:
+    tags = [t.name for t in meta.tags]
+    if len(tags) == 0:
+        tags_str = ""
+    else:
+        tags_str = ", ".join(tags)
+    category = meta.category.name if meta.category else ""
+    row = [str(meta.id), string_shorter(meta.title), category, string_shorter(tags_str)]
+    row_color = color_factory(ArticleStatus(meta.status))
+    row = [color_text(r, row_color) for r in row]
+    return row
+
+
 def generate_meta_output(metas: List[Meta]) -> List[List[str]]:
     output = [["ID", "Title", "Category", "Tags"]]
     for i in metas:
-        tags = [t.name for t in i.tags]
-        if len(tags) == 0:
-            tags_str = ""
-        else:
-            tags_str = ", ".join(tags)
-        category = i.category.name if i.category else ""
-        row = [str(i.id), string_shorter(i.title), category, string_shorter(tags_str)]
+        row = make_meta_row(i)
         output.append(row)
     return output
